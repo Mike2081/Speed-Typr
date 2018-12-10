32cfd5c438d96bb616e99503ee84547a49fa6009
Michael DoHo <michaeldoho@hotmail.com>
2018-12-10 18:11:19 -0500

    first submit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '../src/Assets/Styles/index.css';
+import App from './Assets/Javascript/App';
+import * as serviceWorker from './Assets/Javascript/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
