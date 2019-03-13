bd96ab0e0c9cbff31f3dbf966a06ad6f7a554347
Michael DoHo <michaeldoho@hotmail.com>
2019-03-13 15:41:00 -0400

    rearranged app.use bodyParser methods in server.js

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const app = express();
 const randomWords = require('random-words');
 const path = require('path'); 
 app.use(express.static(path.join(__dirname, 'App')));
-app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 // Must add cause heroku dissproves 'app.use(bodyParser.urlencoded)'. You must call the methods separately
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//extended config needs to be set to true because it has no default
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
