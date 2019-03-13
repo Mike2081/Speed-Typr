c065f9feec44eb42258bbffd9955271844ee003a
Michael DoHo <michaeldoho@hotmail.com>
2019-03-13 17:43:58 -0400

    restructured folders. Added to front-end/back-end paths to app inside of server.js . Also added Heroku post build scripts to package.json

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,15 @@
-const PORT = process.env.PORT || 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+// ^ Needed so the url can be set to heroku's url
 const randomWords = require('random-words');
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, '../front-end/back-end')));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../front-end/back-end', 'index.html'));
+});
+// ^ Folder setup for heroku
 app.use(bodyParser());
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -14,6 +20,6 @@ app.all('/*', function(req, res, next) {
 app.get('/page', (req, res) =>{
     res.json(randomWords({min:1, max:1}));
   });
-app.listen(8080, () => {
-  console.log('server runnning!');
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}!`);
 });
